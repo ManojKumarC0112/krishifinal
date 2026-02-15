2c4a04dd81e105908329ce0dd21d703d4cb0f7cd
ManojKumarC0112 <manojkchandrashekar@gmail.com>
2026-02-15 20:59:22 +0530

    ready for production

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn krishi_sakhi.wsgi
--- krishi_sakhi/settings.py
+++ krishi_sakhi/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s6a&jgl9c$f&2c57lugt3358buvy&c!-*@_g-l+bn&))fk$8lr'
-
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-s6a&jgl9c$f&2c57lugt3358buvy&c!-*@_g-l+bn&))fk$8lr')
+DEBUG = 'RENDER' not in os.environ
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+ 
 
 ALLOWED_HOSTS = ['*']
 
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,6 +124,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
